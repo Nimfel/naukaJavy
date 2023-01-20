3411fc41007dea18813137d0392cd96becbe1680
Nimfel <mariuszpiatek77@gmail.com>
2023-01-20 23:53:02 +0100

    utworzenie matematyka z umiejętnością dodawania

---
+++ src/dzienPierwszy/Matematyk.java
@@ -0,0 +1,10 @@
+package dzienPierwszy;
+
+public class Matematyk {
+
+
+    public double add(double firstParameter, double secondParameter) {
+        return  firstParameter + secondParameter;
+    }
+
+}
---
+++ src/dzienPierwszy/MatematykMain.java
@@ -0,0 +1,21 @@
+package dzienPierwszy;
+
+public class MatematykMain {
+
+    //psvm
+    public static void main(String[] args) {
+
+        Matematyk blondynka = new Matematyk();
+
+//        blondynka.add(5.8, 6666.8);
+        double wynikDodawania = blondynka.add(5.8, 6666.8);
+
+        System.out.println(wynikDodawania);
+
+
+    }
+
+
+
+
+}
