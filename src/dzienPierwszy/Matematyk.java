ba4383f45683bd594ae4e44f98ec822c41486ee2
Nimfel <mariuszpiatek77@gmail.com>
2023-01-22 15:52:32 +0100

    pole trójkąta (2)

--- src/dzienPierwszy/Matematyk.java
+++ src/dzienPierwszy/Matematyk.java
@@ -5,8 +5,8 @@ public class Matematyk {
     public double multiply (double firstParameter, double secondParameter){
         return firstParameter * secondParameter;
     }
-        public double division (double firstParameter * double secondParameter) {
-        return thirdParameter = (firstParameter * secondParameter)
+        public double division (double firstParameter, double secondParameter) {
+        return (firstParameter * secondParameter) /2 ;
 
         }
 
--- src/dzienPierwszy/MatematykMain.java
+++ src/dzienPierwszy/MatematykMain.java
@@ -7,9 +7,8 @@ public class MatematykMain {
 
         Matematyk blondynka = new Matematyk();
 
-
         double multiply = blondynka.multiply(3, 5);
-        double division = blondynka.division(15);
+        double division = blondynka.division((3*5),')'2);
 
         System.out.println("pole trójkąta wynosi:");
         System.out.println(division);
